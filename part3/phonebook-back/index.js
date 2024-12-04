2063ea340ed1c808517f17fe98fc0132e53fb58e
matija grbus <matijagrbus@yahoo.com>
2024-12-04 18:12:46 +0100

    mikji

--- part3/phonebook-back/index.js
+++ part3/phonebook-back/index.js
@@ -6,6 +6,8 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 app.use(morgan('tiny'))
+app.use(express.static('dist'))
+
 morgan.token('type', ((req,res) => {
     if (req.method==='POST') {
          console.log(req.body)
